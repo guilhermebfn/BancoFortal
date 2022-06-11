67bf1149211fe247fd33f0d5538e5fd89937e264
Guilherme <guilhermebfneto@gmail.com>
2022-06-11 11:03:39 -0300

    Commit inicial

---
+++ src/main/java/com/guilherme/bancofortal/BancoFortalApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.bancofortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoFortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoFortalApplication.class, args);
+	}
+
+}
