dbe7ff62c1992596a31ee7ce55a01a76e0a7d5a5
Guilherme <guilhermebfneto@gmail.com>
2022-07-11 16:49:35 -0300

    Retirada do CommandLineRunner.

--- src/main/java/com/guilherme/bancofortal/BancoFortalApplication.java
+++ src/main/java/com/guilherme/bancofortal/BancoFortalApplication.java
@@ -1,27 +1,10 @@
 package com.guilherme.bancofortal;
 
-import com.guilherme.bancofortal.entidades.Cliente;
-import com.guilherme.bancofortal.repositorios.RepoCliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BancoFortalApplication {
-
-	@Bean
-	public CommandLineRunner teste(@Autowired RepoCliente repoCliente) {
-		return args -> {
-
-			String oi = "oi";
-
-			System.out.println(oi);
-
-			repoCliente.save(new Cliente("guilherme"));
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BancoFortalApplication.class, args);
