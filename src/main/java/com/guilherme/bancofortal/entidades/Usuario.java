d344e75b1fa111fb89b4962c044fc0c2b1be1c0c
Guilherme <guilhermebfneto@gmail.com>
2022-08-12 21:34:12 -0300

    Criação de método de busca findByNomeUsuario em RepoUsuario e validação e criação de construtor, getters e setters em Usuario.

--- src/main/java/com/guilherme/bancofortal/entidades/Usuario.java
+++ src/main/java/com/guilherme/bancofortal/entidades/Usuario.java
@@ -1,6 +1,7 @@
 package com.guilherme.bancofortal.entidades;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table
@@ -11,8 +12,33 @@ public class Usuario {
     private Integer id;
 
     @Column(name = "nome_usuario")
+    @NotEmpty(message = "{campo.nome-usuario.obrigatorio}")
     private String nomeUsuario;
 
     @Column
+    @NotEmpty(message = "{campo.senha.obrigatorio}")
     private String senha;
+
+    public Usuario(Integer id, String nomeUsuario, String senha) {
+        this.id = id;
+        this.nomeUsuario = nomeUsuario;
+        this.senha = senha;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
--- src/main/java/com/guilherme/bancofortal/repositorios/RepoUsuario.java
+++ src/main/java/com/guilherme/bancofortal/repositorios/RepoUsuario.java
@@ -4,6 +4,10 @@ import com.guilherme.bancofortal.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RepoUsuario extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByNomeUsuario(String nomeUsuario);
 }
